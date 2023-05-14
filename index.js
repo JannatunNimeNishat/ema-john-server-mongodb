5c05c8708e46b4cf54cade818a123734051fb7e6
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-14 22:12:07 +0600

    api - get all products api added

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
 
+    app.get('/products', async(req,res)=>{
+        const result = await productCollection.find().toArray()
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
